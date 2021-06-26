0e608f5e1241aacf921d3c73cd516fdf6be22a5f
wuliehao00 <397385590@qq.com>
2021-06-26 19:54:18 +0800

    Update Note01.java

--- src/com/huawei/study01/Note01.java
+++ src/com/huawei/study01/Note01.java
@@ -3,5 +3,6 @@ package com.huawei.study01;
 public class Note01 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("这是我增加的一部分");
     }
 }
