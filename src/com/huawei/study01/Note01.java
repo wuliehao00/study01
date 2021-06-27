efac1296fc9be3399551125238ce970bd21f60d9
Administrator <397385590@qq.com>
2021-06-27 22:22:31 +0800

    提交代码

--- src/com/huawei/study01/Note01.java
+++ src/com/huawei/study01/Note01.java
@@ -2,7 +2,10 @@ package com.huawei.study01;
 
 /**
  * java语法
- * 1. .java文件编译成class文件
+ * 1. .java文件编译成class文件  --  javac.exe
+ *     运行java脚本    java.exe
+ *     先启动java虚拟机，然后虚拟机启动类加载器，加载器去寻找类，然后装进来编
+ *      译成二进制文件与底层硬件交互
  */
 public class Note01 {
     public static void main(String[] args) {
